d1ca4340b6d7146b6e5de1c0cea738b79161e426
Tim Miller <innerlogic4321@gmail.com>
2016-02-11 13:22:04 -0500

    Add Fitbit Stuff

---
+++ DiaperChrisFitbitMap/DiaperChrisFitbitMap/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GleamTech.VideoUltimate;
+using Newtonsoft.Json;
+using ImageFormat = System.Drawing.Imaging.ImageFormat;
+
+namespace DiaperChrisFitbitMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            using (var videoFrameReader = new VideoFrameReader(@"1.mp4"))
+            {
+                var startTime = 97;
+                videoFrameReader.Seek(startTime);
+                var frameIndex = 0;
+                var fitbitRates = new List<FitbitRate>();
+                foreach (var frame in videoFrameReader)
+                {
+                    using (frame)
+                    {
+                        if (frameIndex % 60 == 0) //Save every 60th frame
+                        {
+                            Console.WriteLine($"Getting image at {TimeSpan.FromSeconds(startTime).TotalMinutes}");
+                            var image = CropImage(frame, new Rectangle(30, 10, 30, 20));
+                            //var ocrImage = new ImageReader(ImageToStream(image, ImageFormat.Jpeg));
+                            //Console.WriteLine("Wait a sec...");
+                            //await Task.Delay(5000);
+                            //var result = await ocrImage.GetImageAnalysisResultsAsync();
+                            //if (result != null && result.Regions.Any())
+                            //{
+                            //    if (result.Regions.First().Lines.Any())
+                            //    {
+                            //        if (result.Regions.First().Lines.First().Words.Any())
+                            //        {
+                            //            var word = result.Regions.First().Lines.First().Words.First();
+                            //            try
+                            //            {
+                            //                var wordInt = Convert.ToInt32(word.Text);
+                            //                fitbitRates.Add(new FitbitRate()
+                            //                {
+                            //                    HeartRate = wordInt,
+                            //                    Time = TimeSpan.FromSeconds(startTime).TotalMinutes
+                            //                });
+                            //            }
+                            //            catch (Exception)
+                            //            {
+                            //                // If we failed to parse, add in the last file.
+                            //                if (fitbitRates.Any())
+                            //                {
+                            //                    fitbitRates.Add(fitbitRates.Last());
+                            //                }
+                            //                Console.WriteLine($"Fail at {startTime}");
+                            //            }
+                            //        }
+                            //    }
+                            //}
+                            
+                            // Save debug image
+                            //image.Save(string.Format(@"1/Frame{0} - {1}.jpg", frameIndex, TimeSpan.FromSeconds(startTime).TotalMinutes));
+                            //image.Dispose();
+                            // Save full image
+                            frame.Save($@"1-full/Frame{frameIndex} - {startTime} - {TimeSpan.FromSeconds(startTime).TotalMinutes}.jpg", ImageFormat.Jpeg);
+                            startTime++;
+                        }
+                        frameIndex++;
+                    }
+                }
+
+                File.WriteAllText("output-1.txt", JsonConvert.SerializeObject(fitbitRates));
+            }
+        }
+
+        public class FitbitRate
+        {
+            public int HeartRate { get; set; }
+            public double Time { get; set; }
+        }
+
+        private static Image CropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            bmpImage = bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+            return new Bitmap(bmpImage, new Size(90, 60));
+        }
+
+        public static Stream ImageToStream(Image image, ImageFormat formaw)
+        {
+            var stream = new MemoryStream();
+            image.Save(stream, formaw);
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ DiaperChrisFitbitUwp/DiaperChrisFitbitUwp/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DiaperChrisFitbitUwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private OcrEngine ocrEngine;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Language ocrLanguage = new Language("en");
+            ocrEngine = OcrEngine.TryCreateFromLanguage(ocrLanguage);
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fitbitRates = new List<FitbitRate>();
+            var failCount = 0;
+            StorageFolder pictureFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets\1-full");
+            IReadOnlyList<StorageFile> fileList = await pictureFolder.GetFilesAsync();
+            foreach (StorageFile file in fileList)
+            {
+                var filename = file.DisplayName;
+                var splitfilename = filename.Split('-');
+
+                var writeableBitmap = await CropBitmap.GetCroppedBitmapAsync(file, new Point(30, 10), new Size(30, 20), 2);
+                var result = await ocrEngine.RecognizeAsync(SoftwareBitmap.CreateCopyFromBuffer(
+                    writeableBitmap.PixelBuffer,
+                    BitmapPixelFormat.Bgra8,
+                    writeableBitmap.PixelWidth,
+                    writeableBitmap.PixelHeight
+                    ));
+                try
+                {
+                    var wordInt = Convert.ToInt32(new String(result.Text.Where(Char.IsDigit).ToArray()));
+                    var starttime = Convert.ToInt32(splitfilename[1].Trim());
+                    fitbitRates.Add(new FitbitRate()
+                    {
+                        StartTime = starttime,
+                        HeartRate = wordInt,
+                        Time = TimeSpan.FromSeconds(starttime).TotalMinutes,
+                        FileName = file.Name
+                    });
+                }
+                catch (Exception ex)
+                {
+                    //TestImage.Source = writeableBitmap;
+                    //break;
+                    if (fitbitRates.Any())
+                    {
+
+                        var last = fitbitRates.Last();
+                        fitbitRates.Add(new FitbitRate()
+                        {
+                            HeartRate = last.HeartRate,
+                            StartTime = last.StartTime + 1,
+                            Time = TimeSpan.FromSeconds(last.StartTime).TotalMinutes,
+                            FileName = file.Name
+                        });
+                    }
+                    failCount = failCount + 1;
+                }
+
+            }
+            OutputBox.Text = JsonConvert.SerializeObject(fitbitRates, Formatting.Indented);
+        }
+
+
+        public class FitbitRate
+        {
+            public int HeartRate { get; set; }
+            public double Time { get; set; }
+
+            public int StartTime { get; set; }
+
+            public string FileName { get; set; }
+        }
+    }
+}
