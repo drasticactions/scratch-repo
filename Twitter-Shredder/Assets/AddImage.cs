6d89b60af1a7fd36a2ded25dbc0759ff016f3f7c
Tim Miller <innerlogic4321@gmail.com>
2016-02-17 17:25:38 -0500

    Update Build

---
+++ Twitter-Shredder/Assets/AddImage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddImage : MonoBehaviour
+{
+    private string url = "https://pbs.twimg.com/profile_images/557282175452581888/a9ik7C4m.png";
+    private Texture2D img;
+    private Renderer _renderer;
+	// Use this for initialization
+	void Start ()
+	{
+        _renderer = GetComponent<Renderer>();
+	    StartCoroutine(LoadImage());
+	}
+
+    IEnumerator LoadImage()
+    {
+        yield return 0;
+        var imgLink = new WWW(url);
+        yield return imgLink;
+        img = imgLink.texture;
+        _renderer.material.mainTexture = img;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Twitter-Shredder/Assets/DumbButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DumbButton : MonoBehaviour
+{
+    public int minRange = 1;
+    private GameObject _player;
+    private bool _isAnimationPlaying;
+    // Use this for initialization
+    void Start ()
+	{
+	    _player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_isAnimationPlaying)
+	    {
+	        return;
+	    }
+
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
+        bool closeEnough = distance < minRange;
+	    if (closeEnough)
+	    {
+	        if (Input.GetKeyDown(KeyCode.E) && !_isAnimationPlaying)
+	        {
+	            _isAnimationPlaying = true;
+            }
+	    }
+    }
+}
